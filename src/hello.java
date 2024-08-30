fddf0004fe9b17ae58c36bbd9a045bc5b9ced6b8
fcyza <fcyza@Fishers_Laptop.unl.edu>
2024-08-30 16:50:14 -0500

    Initial Commit

---
+++ src/hello.java
@@ -0,0 +1,16 @@
+
+/**
+ * Author: Fisher Cyza (fcyza2@huskers.unl.edu) 
+ * Date: 08/30/2024
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author SeekerOfCode00
+ *
+ */
+public class hello {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World!");
+	}
+}
